7e68d5bbb286423ef0a08b1936035524b046cc59
Rodrigo Wirth <rodrigo.wirth@bravi.com.br>
2017-07-16 19:29:34 -0300

    Authentication

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
 const express = require('express');
-
 // This package automatically parses JSON requests.
 const bodyParser = require('body-parser');
-
 // This package will handle GraphQL server requests and responses
 // for you, based on your schema.
 const {graphqlExpress, graphiqlExpress} = require('graphql-server-express');
 
+const {authenticate} = require('./authentication');
 const schema = require('./schema');
 
 const connectMongo = require('./mongo-connector');
 
 const start = async () => {
-  // 3
   const mongo = await connectMongo();
   var app = express();
 
-  app.use('/graphql', bodyParser.json(), graphqlExpress({
-    context: { mongo }, // 4
-    schema
-  }));
+  const buildOptions = async (req, res) => {
+    const user = await authenticate(req, mongo.Users);
+    return {
+      context: {mongo, user}, // This context object is passed to all resolvers.
+      schema,
+    };
+  };
+  app.use('/graphql', bodyParser.json(), graphqlExpress(buildOptions));
 
   app.use('/graphiql', graphiqlExpress({
     endpointURL: '/graphql',
+    passHeader: `'Authorization': 'bearer token-maria.bello@vtex.com'`,
   }));
 
   const PORT = 3000;
--- src/schema/index.js
+++ src/schema/index.js
@@ -4,18 +4,43 @@ const resolvers = require('./resolvers');
 
 // Define your types here.
 const typeDefs = `
-  type Link {
-    id: ID!
-    url: String!
-    description: String!
-  }
-
   type Query {
     allLinks: [Link!]!
   }
 
   type Mutation {
     createLink(url: String!, description: String!): Link
+
+    createUser(name: String!, authProvider: AuthProviderSignupData!): User
+
+    signinUser(email: AUTH_PROVIDER_EMAIL): SigninPayload!
+  }
+
+  type Link {
+    id: ID!
+    url: String!
+    description: String!
+    postedBy: User
+  }
+
+  type User {
+      id: ID!
+      name: String!
+      email: String
+  }
+
+  type SigninPayload {
+    token: String
+    user: User
+  }
+
+  input AuthProviderSignupData {
+    email: AUTH_PROVIDER_EMAIL
+  }
+
+  input AUTH_PROVIDER_EMAIL {
+      email: String!
+      password: String!
   }
 `;
 
