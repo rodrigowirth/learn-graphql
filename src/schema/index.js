263167ea48da242728fe0708e9c31867a5f8719e
Rodrigo Wirth <rodrigo.wirth@bravi.com.br>
2017-07-16 15:22:10 -0300

    Queries

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,5 +1,7 @@
 const {makeExecutableSchema} = require('graphql-tools');
 
+const resolvers = require('./resolvers');
+
 // Define your types here.
 const typeDefs = `
   type Link {
@@ -7,7 +9,11 @@ const typeDefs = `
     url: String!
     description: String!
   }
+
+  type Query {
+    allLinks: [Link!]!
+  }
 `;
 
 // Generate the schema object from your types definition.
-module.exports = makeExecutableSchema({typeDefs});
+module.exports = makeExecutableSchema({typeDefs, resolvers});
