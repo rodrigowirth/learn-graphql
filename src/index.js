e089cc0e9110234bb546577fb24bece2c0bec2cb
Rodrigo Wirth <rodrigo.wirth@bravi.com.br>
2017-07-16 19:05:16 -0300

    Connectors

--- src/index.js
+++ src/index.js
@@ -9,14 +9,27 @@ const {graphqlExpress, graphiqlExpress} = require('graphql-server-express');
 
 const schema = require('./schema');
 
-var app = express();
-app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
+const connectMongo = require('./mongo-connector');
 
-app.use('/graphiql', graphiqlExpress({
-  endpointURL: '/graphql',
-}));
+const start = async () => {
+  // 3
+  const mongo = await connectMongo();
+  var app = express();
 
-const PORT = 3000
-app.listen(PORT, () => {
-  console.log(`Hackernews GraphQL server running on port ${PORT}.`)
-});
+  app.use('/graphql', bodyParser.json(), graphqlExpress({
+    context: { mongo }, // 4
+    schema
+  }));
+
+  app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql',
+  }));
+
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`Hackernews GraphQL server running on port ${PORT}.`)
+  });
+};
+
+// 5
+start();
--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -13,13 +13,19 @@ const links = [
 
 module.exports = {
   Query: {
-    allLinks: () => links,
+    allLinks: async (root, data, {mongo: {Links}}) => { // 1
+      return await Links.find({}).toArray(); // 2
+    },
   },
+
   Mutation: {
-   createLink: (_, data) => {
-     const newLink = Object.assign({id: links.length}, data);
-     links.push(newLink);
-     return newLink;
-   }
- },
+    createLink: async (root, data, {mongo: {Links}}) => {
+      const response = await Links.insert(data); // 3
+      return Object.assign({id: response.insertedIds[0]}, data); // 4
+    },
+  },
+
+  Link: {
+    id: root => root._id || root.id, // 5
+  },
 };
